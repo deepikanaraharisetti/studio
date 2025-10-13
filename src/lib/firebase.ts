c602a44875b216b87f96fb35768cb1a040d65a15
PRASHANTH <prashanth49kaki@gmail.com>
2025-10-13 16:32:09 +0000

    still i can't sign up

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,4 +1,5 @@
 
+import 'dotenv/config';
 import { initializeApp, getApps, getApp, FirebaseOptions } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
