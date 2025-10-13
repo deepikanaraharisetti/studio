f336409dfbe7c82512c0a239ac75c8484c0c252c
PRASHANTH <prashanth49kaki@gmail.com>
2025-10-13 16:44:35 +0000

    i'm currently running it in firebase studio itself

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,19 +1,17 @@
 
-import 'dotenv/config';
 import { initializeApp, getApps, getApp, FirebaseOptions } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 // Your web app's Firebase configuration
-// IMPORTANT: Replace with your own Firebase project configuration.
 const firebaseConfig: FirebaseOptions = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyAITa6yIQd2NWdrQVXG62Pd_BLoixG5eM8",
+  authDomain: "studio-1373398294-3404b.firebaseapp.com",
+  projectId: "studio-1373398294-3404b",
+  storageBucket: "studio-1373398294-3404b.appspot.com",
+  messagingSenderId: "73356956617",
+  appId: "1:73356956617:web:73c7cc1b1fb64476be4e18"
 };
 
 // Initialize Firebase
