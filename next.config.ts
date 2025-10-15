c6b2dd0807d812386ec809567a086bd7fbff99b2
Deepika Naraharisetti <deepikanaraharisettii@gmail.com>
2025-10-15 17:28:48 +0000

    Try fixing this error: `Runtime Error: Error: Page "/(app)/users/[uid]/p

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import 'dotenv/config';
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  output: 'export',
+  output: 'standalone',
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
